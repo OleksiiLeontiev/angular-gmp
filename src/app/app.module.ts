486e7fdf46cfc4eb61a3097ad17ac69600268553
OleksiiLeontiev <oleksii_leontiev@ukr.net>
2022-04-25 17:31:37 +0300

    component and interfaces moved from core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { CoursesItemComponent } from './components/courses-item/courses-item.component';
+import { CoursesListComponent } from './components/courses-list/courses-list.component';
+import { CoursesPageComponent } from './components/courses-page/courses-page.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    FooterComponent,
+    CoursesListComponent,
+    CoursesItemComponent,
+    LogoComponent,
+    MenuComponent,
+    SearchFormComponent,
+    CoursesPageComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,33 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CoursesListComponent } from './components/courses-list/courses-list.component';
-import { CoursesItemComponent } from './components/courses-item/courses-item.component';
-import { LogoComponent } from './components/logo/logo.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { SearchFormComponent } from './components/search-form/search-form.component';
-import { CoursesPageComponent } from './components/courses-page/courses-page.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
-    CoursesListComponent,
-    CoursesItemComponent,
-    LogoComponent,
-    MenuComponent,
-    SearchFormComponent,
-    CoursesPageComponent,
-  ],
-  exports: [
-    HeaderComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
-    CoursesPageComponent,
-  ],
+  declarations: [],
+  exports: [],
   imports: [CommonModule],
 })
 export class CoreModule {}
