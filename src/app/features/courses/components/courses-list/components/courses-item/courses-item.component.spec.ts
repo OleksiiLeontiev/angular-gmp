4bb00bb790d3bceb0656681e6178ce216ffba2e5
OleksiiLeontiev <oleksii_leontiev@ukr.net>
2022-05-13 13:10:47 +0300

    hw3 - unit tests

--- src/app/features/courses/components/courses-list/components/courses-item/courses-item.component.spec.ts
+++ src/app/features/courses/components/courses-list/components/courses-item/courses-item.component.spec.ts
@@ -1,10 +1,14 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CoursesItemComponent } from './courses-item.component';
 
 describe('CoursesItemComponent', () => {
   let component: CoursesItemComponent;
   let fixture: ComponentFixture<CoursesItemComponent>;
+  let deleteButton: DebugElement;
+  let editButton: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +19,49 @@ describe('CoursesItemComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CoursesItemComponent);
     component = fixture.componentInstance;
+
+    component.course = {
+      id: 1,
+      title: 'Video Course 1. Name tag',
+      creationDate: '08/28/2020',
+      duration: 88,
+      description:
+        "Learn about where you can find course descriptions, what information they include, how they work, and details about various components of a course description. Course descriptions report information about a university or college's classes. They're published both in course catalogs that outline degree requirements and in course schedules that contain descriptions for all courses offered during a particular semester.",
+    };
+
+    deleteButton = fixture.debugElement.query(By.css('.jasmine-delete-button'));
+    editButton = fixture.debugElement.query(By.css('.jasmine-edit-button'));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should raise deleteClick event when clicked deleteButton', () => {
+    const editEventSpy = spyOn(component.deleteEvent, 'emit');
+
+    deleteButton.triggerEventHandler('click', null);
+    expect(editEventSpy).toHaveBeenCalled();
+  });
+
+  it('should raise editClick event when clicked editButton', () => {
+    const editEventSpy = spyOn(component.editEvent, 'emit');
+
+    editButton.triggerEventHandler('click', null);
+    expect(editEventSpy).toHaveBeenCalled();
+  });
+
+  it('getFormattedDuration works with hours and minutes', () => {
+    expect(component.getFormattedDuration(88)).toEqual('1h 28min');
+  });
+
+  it('getFormattedDuration works with only hours', () => {
+    expect(component.getFormattedDuration(120)).toEqual('2h ');
+  });
+
+  it('getFormattedDuration works with only minutes', () => {
+    expect(component.getFormattedDuration(59)).toEqual('59min');
   });
 });
--- src/app/features/courses/components/courses-search/courses-search.component.spec.ts
+++ src/app/features/courses/components/courses-search/courses-search.component.spec.ts
@@ -1,10 +1,13 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { CoursesSearchComponent } from './courses-search.component';
 
 describe('CoursesSearchComponent', () => {
   let component: CoursesSearchComponent;
   let fixture: ComponentFixture<CoursesSearchComponent>;
+  let searchButton: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,10 +18,28 @@ describe('CoursesSearchComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CoursesSearchComponent);
     component = fixture.componentInstance;
+
+    searchButton = fixture.debugElement.query(By.css('.jasmine-search-button'));
+    component.searchValue = 'test';
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should trigger searchClick on search button click', () => {
+    const searchClickSpy = spyOn(component, 'searchClick');
+    searchButton.triggerEventHandler('click', null);
+
+    expect(searchClickSpy).toHaveBeenCalled();
+  });
+
+  it('searchClick works', () => {
+    const consoleSpy = spyOn(console, 'log');
+    component.searchClick();
+
+    expect(consoleSpy).toHaveBeenCalledWith('test');
   });
 });
