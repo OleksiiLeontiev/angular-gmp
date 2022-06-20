27132a3b58d79c46ba4dc980c44009855212cb16
OleksiiLeontiev <oleksii_leontiev@ukr.net>
2022-06-20 20:16:41 +0300

    form creation moved to separate function in course-edit component

--- src/app/features/courses/components/courses-edit/courses-edit.component.ts
+++ src/app/features/courses/components/courses-edit/courses-edit.component.ts
@@ -9,22 +9,14 @@ import { Course } from '../../models/course';
   templateUrl: './courses-edit.component.html',
   styleUrls: ['./courses-edit.component.scss'],
 })
-export class CoursesEditComponent implements OnInit {
+export class CoursesEditComponent {
   private operationType: 'edit' | 'create' = 'create';
 
   get pageTitle(): string {
     return this.operationType === 'edit' ? 'Edit Course' : 'New Course';
   }
 
-  public courseForm: FormGroup = this.fb.group({
-    id: Date.now(),
-    title: '',
-    description: '',
-    duration: 60,
-    creationDate: '',
-    topRated: false,
-    authors: '',
-  });
+  public courseForm!: FormGroup;
 
   constructor(
     private coursesService: CoursesService,
@@ -33,19 +25,8 @@ export class CoursesEditComponent implements OnInit {
     private fb: FormBuilder
   ) {
     const courseId = this.route.snapshot.params['id'];
-
-    if (courseId) {
-      const currentCourse = this.coursesService.getCourseById(+courseId);
-      if (currentCourse)
-        this.courseForm.setValue({
-          ...currentCourse,
-          authors: '',
-        });
-      this.operationType = 'edit';
-    }
+    this.courseForm = this.createForm(courseId);
   }
-
-  ngOnInit(): void {}
 
   onSave() {
     if (this.operationType === 'edit') {
@@ -54,5 +35,29 @@ export class CoursesEditComponent implements OnInit {
       this.coursesService.createCourse(this.courseForm.value);
     }
     this.router.navigate(['/courses']);
+  }
+
+  createForm(courseId: number): FormGroup {
+    if (courseId) {
+      const currentCourse = this.coursesService.getCourseById(+courseId);
+      if (currentCourse) {
+        this.operationType = 'edit';
+
+        return this.fb.group({
+          ...currentCourse,
+          authors: '',
+        });
+      }
+    }
+
+    return this.fb.group({
+      id: Date.now(),
+      title: '',
+      description: '',
+      duration: 60,
+      creationDate: '',
+      topRated: false,
+      authors: '',
+    });
   }
 }
